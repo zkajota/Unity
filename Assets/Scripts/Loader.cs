5ca924bff457ab7069cafeebe985c566902be1cb
zkajota <zkajota@gmail.com>
2018-04-26 10:32:48 +0100

    Reupload assets

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml.Serialization;
+
+using System.Text;
+
+public class Loader : MonoBehaviour
+{
+
+    /***************************      Save NPC To XML     ***********************************************************/
+    public static void ExportNPCs(List<NPCClass> npcList)
+    {
+        foreach (var item in npcList)
+        {
+            string file = Application.dataPath + "/Data/NPCs/" + item.name + ".xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(NPCClass));
+
+            using (StreamWriter sw1 = new StreamWriter(file, false, Encoding.GetEncoding("ISO-8859-2")))
+            {
+                serializer.Serialize(sw1, item);
+            }
+        }
+
+    }
+
+    /***************************      LOAD NPCs   ***********************************************************/
+    public static void ImportNPCs()
+    {
+        string dirpath = Application.dataPath + "/Data/NPCs";
+        DirectoryInfo d = new DirectoryInfo(dirpath);
+
+        foreach (var item in d.GetFiles("*.xml"))
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(NPCClass));
+            string file = item.FullName;
+            using (StreamReader sw1 = new StreamReader(file, Encoding.GetEncoding("ISO-8859-2")))
+            {
+                NPCClass _npc = serializer.Deserialize(sw1) as NPCClass;
+                GameManager.instance.npcList.Add(_npc);
+                
+            }
+
+        }
+        Debug.Log("Load Complete -  NPC amount: " + GameManager.instance.npcList.Count);
+    }
+
+
+
+    //// NEEDS
+
+    /***************************      Save NEEDS to XML     ***********************************************************/
+    public static void ExportNeeds(List<NeedClass> needsList)
+    {
+        foreach (var item in needsList)
+        {
+            string plik = Application.dataPath + "/Data/Needs/" + item.name + ".xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(NeedClass));
+
+            using (StreamWriter sw1 = new StreamWriter(plik, false, Encoding.GetEncoding("ISO-8859-2")))
+            {
+                serializer.Serialize(sw1, item);
+            }
+        }
+
+    }
+
+    public static void ImportNeeds()
+    {
+        string dirpath = Application.dataPath + "/Data/Needs";
+        DirectoryInfo d = new DirectoryInfo(dirpath);
+
+        foreach (var item in d.GetFiles("*.xml"))
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(NeedClass));
+            string file = item.FullName;
+            using (StreamReader sw1 = new StreamReader(file, Encoding.GetEncoding("ISO-8859-2")))
+            {
+                NeedClass _need = serializer.Deserialize(sw1) as NeedClass;
+                GameManager.instance.needsList.Add(_need);
+
+            }
+
+        }
+        Debug.Log("Load Complete -  Needs amount: " + GameManager.instance.needsList.Count);
+    }
+
+
+    //// Actions
+
+    /***************************      Save Actions to XML (Serialisation)    ***********************************************************/
+    public static void ExportActions(List<ActionClass> actions)
+    {
+        foreach (var item in actions)
+        {
+            string file = Application.dataPath + "/Data/Actions/" + item.name + ".xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(ActionClass));
+
+            using (StreamWriter sw1 = new StreamWriter(file, false, Encoding.GetEncoding("ISO-8859-2")))
+            {
+                serializer.Serialize(sw1, item);
+            }
+        }
+
+    }
+
+    public static void ImportActions()
+    {
+        string dirpath = Application.dataPath + "/Data/Actions";
+        DirectoryInfo d = new DirectoryInfo(dirpath);
+
+        foreach (var item in d.GetFiles("*.xml"))
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ActionClass));
+            string file = item.FullName;
+            using (StreamReader sw1 = new StreamReader(file, Encoding.GetEncoding("ISO-8859-2")))
+            {
+                ActionClass _act = serializer.Deserialize(sw1) as ActionClass;
+                GameManager.instance.actionsList.Add(_act);
+
+            }
+
+        }
+        Debug.Log("Load Complete -  Actions amount: " + GameManager.instance.actionsList.Count);
+    }
+
+
+}
+
---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class NPC : MonoBehaviour {
+
+    public NPCClass npc;
+    public List<ActionClass> actions = new List<ActionClass>();
+    public List<NeedClass> needs = new List<NeedClass>();
+    public List<ActionClass> queue = new List<ActionClass>();
+    Transform area;
+    private bool fulfilling;
+    Transform target;
+    NavMeshAgent agent;
+    
+
+    public List<GameObject> availableAreas;
+
+    public NavMeshAgent navmesh;
+
+    void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        target = null;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        AssignNeeds();
+        AssignArea();
+        AssignActions();
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        AddToQueue();
+        DoQueue();
+        TickNeeds();
+        if (target != null)
+        {
+            MoveTo(target);
+        }
+    }
+
+  
+
+
+    
+
+
+    void AssignNeeds()
+    {
+        foreach (NeedClass item in GameManager.instance.needsList)
+        {
+            if (npc.needList.Contains(item.name))
+            {
+                needs.Add((NeedClass)item.Clone());
+            }
+        }
+    }
+
+    void AssignActions()
+    {
+        actions = GameManager.instance.actionsList;
+
+        
+    }
+
+    void AssignArea()
+    {
+        foreach (var item in GameManager.instance.areas)
+        {
+            if (item.name == npc.areaName)
+            {
+                area = item;
+                break;
+            }
+        }
+    }
+
+    private void AddToQueue()
+    {
+        foreach (var item in needs)
+        {
+            if (item.needValue > 20)
+            {
+                //if need is not already in queue
+                if (item.queued == false)
+                {
+                    //find which action to fulfill and add to queue
+                    foreach (var action in actions)
+                    {
+                            if (action.forNeed == item.name)
+                            {
+                                Transform aa = action.CheckPlaces(area);
+                                if (aa != null)
+                                {
+                                    queue.Add(action);
+                                    UI_Script.instance.CreateActionsUI();
+                                    item.queued = true;
+                                }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void DoQueue()
+    {
+        //check if any item is in Queue
+        if (queue.Count != 0)
+        {
+            ActionClass currentAction = queue[0];
+            if (target == null)
+            {
+                //find which fountain to go to. If all are taken shuffle Queue.
+                target = currentAction.FindActionplace(area);
+                if (target == null)
+                {
+                    if (queue.Count > 1)
+                    {
+                        ShuffleQueue();
+
+                    }
+                }
+            }
+            if (target != null)
+            {
+                //check if close to correct fountain, and fulfill need
+                if (Vector3.Distance(transform.position, target.position) < 1.0f)
+                {
+                    foreach (var need in needs)
+                    {
+                        if (need.name == currentAction.forNeed)
+                        {
+                            bool finished = currentAction.FulfillNeed(need, true, target);
+                            gameObject.GetComponent<Renderer>().material.SetColor("_Color", currentAction.color);
+                            target.GetComponent<ActionObjectScript>().isTaken = true;
+
+                            //remove from queue
+                            if (finished == true)
+                            {
+                                need.queued = false;
+                                target.GetComponent<ActionObjectScript>().isTaken = false;
+
+                                target = null;
+                                queue.RemoveAt(0);
+                                gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                                UI_Script.instance.CreateActionsUI();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    private void MoveTo(Transform _target)
+    {
+        agent.SetDestination(_target.position);
+        Debug.Log("I'm moving to - " + _target.name);
+    }
+
+    private void TickNeeds()
+    {
+        foreach (var need in needs)
+        {
+            need.Tick();
+        }
+    }
+
+    private void ShuffleQueue()
+    {
+        Debug.Log("shuffling");
+        for (int i = 0; i < queue.Count; i++)
+        {
+            ActionClass ac = queue[i];
+            int randomIndex = Random.Range(i, queue.Count);
+            queue[i] = queue[randomIndex];
+            queue[randomIndex] = ac;
+        }
+    }
+}
